8423a5f1a30ff6328c24738ae605fd4b72989530
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-07-17 23:11:36 +0300

    Create 06_problem.cpp

---
+++ 06_problem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+   string num ;
+  
+   cin >> num;
+   
+   int n = stoi(num);
+  
+   cout << n << " " << 3 * n;
+    
+   return 0;
+}
