aca74c806f809d01dc2d5ff0063b440abab14bf5
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-30 18:23:35 +0300

    Update 41_problem.cpp

--- 41_problem.cpp
+++ 41_problem.cpp
@@ -24,3 +24,20 @@ int main() {
     cout << isAlpha() << endl;
     return 0;
 }
+
+// file #2
+
+#include<iostream>
+using namespace std;
+
+bool isLeap(int year){
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+int main() {
+    cout << "Enter a year: " << endl;
+    int y;
+    cin >> y;
+    cout << y  << " " ;
+    cout << (isLeap(y)? "is a leap year" : "is not a leap year") << endl;
+    return 0;
+}
