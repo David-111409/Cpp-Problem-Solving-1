3065ccef13d4b8b3949295329ffaeecbffe507e4
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-01-05 10:13:51 +0200

    Update 44_problem.cpp

--- 44_problem.cpp
+++ 44_problem.cpp
@@ -18,3 +18,82 @@ int main()
 
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, num , max;
+    cin >> N;
+    cin >> num;
+    cin >> max;
+    N = N - 2;
+    
+    if(max < num){
+            max = num;
+        }
+    if (N){
+        cin >> num;
+        if (max < num){
+            max = num;
+        }
+        
+        N--;
+    }
+    
+     if (N){
+        cin >> num;
+        if (max < num){
+            max = num;
+        }
+        
+        N--;
+    }
+    
+     if (N){
+        cin >> num;
+        if (max < num){
+            max = num;
+        }
+        
+        N--;
+    } if (N){
+        cin >> num;
+        if (max < num){
+            max = num;
+        }
+        
+        N--;
+    } if (N){
+        cin >> num;
+        if (max < num){
+            max = num;
+        }
+        
+        N--;
+    } if (N){
+        cin >> num;
+        if (max < num){
+            max = num;
+        }
+        
+        N--;
+    }
+     if (N){
+        cin >> num;
+        if (max < num){
+            max = num;
+        }
+        
+        N--;
+    }
+    
+    
+    
+   
+    cout << "the max number is:" << max;
+
+    return 0;
+}
+*/
