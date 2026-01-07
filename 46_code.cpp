d0a30264aa298cd3df1f955986e871d53df94188
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-01-07 09:16:17 +0200

    Create 46_code.cpp

---
+++ 46_code.cpp
@@ -0,0 +1,83 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     // int rows;
+//     // cin >> rows;
+    
+//     // while(rows){
+//     //     int i = rows;
+        
+//     //     while(i){
+//     //         cout << "*";
+//     //         i--;
+//     //     }
+//     //     cout << endl;
+        
+//     //     rows--;
+//     // }
+    
+//     // int i = 0;
+//     // int k = i++;
+//     // cout << k << endl;
+//     // cout << i ;
+    
+//     int i = 2;
+//     if (i == 1){
+//         cout << "Hello 1" << endl;
+//     }
+//     else {
+//         cout << "Hello not 1" << endl;
+//     }
+    
+//     if(true){
+//         cout << "Hello true\n";
+//     }
+
+//     for (int x = 0; x < 10 ; x++){
+//         cout << x++ << endl;
+//     }
+//     return 0;
+// }
+
+    // #include<iostream>
+    // using namespace std;
+    // int main() {
+    // 	int a = 0, b = 1, c = 2;
+    // 	while(a + b + c < 16)
+    // 	    cout << a + b + c << " ", ++a, b += 2, c += 3;
+    // 	return 0;
+    // }
+
+    #include<iostream>
+    using namespace std;
+     
+    int main() {
+        // for (int i = 5; i > 1; i--, cout << i);
+    // 	for (int a = 0, b = 1, c = 2; a + b + c < 16; ++a, b += 2, c += 3)
+    // 	    cout << a + b + c << " ";
+        // int n;
+        // cin >> n;
+        // int evenSum = 0;
+        // int oddSum = 0;
+        // int cur;
+        // int i = 0;
+        // int k = 0;
+        // while(n){
+        //     cin >> cur;
+        //     if (n % 2){
+        //         evenSum += cur;
+        //         i++;
+        //     }
+        //     else{
+        //         oddSum += cur;
+        //         k++;
+        //     }
+        //     n--;
+        // }
+        
+        // cout << oddSum / k;
+        // cout << " ";
+        // cout << evenSum / i;
+        
+    	return 0;
+    }
