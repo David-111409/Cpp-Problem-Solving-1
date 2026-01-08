2821157b422d93ad2abd4f87558b0c2a34207c75
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-01-08 09:24:48 +0200

    Comment out main function in 48_code.cpp
    Commented out the main function and its logic for user input and output.

--- 48_code.cpp
+++ 48_code.cpp
@@ -45,3 +45,21 @@ int main()
 
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cin >> n;
+    
+    for (int i = 0; i <= n; i++){
+        if (i % 12 == 0 || i % 8 == 0){
+            cout << i << " ";
+        }
+    }
+    
+    return 0;
+}
+*/
