95a4a74907bd256b396ed64a2ff52f15207fc263
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-01-10 14:59:09 +0200

    Update 49_code.cpp

--- 49_code.cpp
+++ 49_code.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 using namespace std;
 
@@ -41,3 +39,50 @@ int main()
    
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{  
+    // int n;
+    // cin >> n;
+    
+    // if (n < 0){
+    //     n = n * -1;
+    // }   
+    
+    // if (n < 10){
+    //     cout << "1 digit";
+    // }
+    // else if (n < 100){
+    //     cout << "2 digits";
+    // }
+    // else if (n < 1000){
+    //     cout << "3 digits";
+    // }
+    // else if (n < 10000){
+    //     cout << "4 digit";
+    // }
+    // else {
+    //     cout << "5 digits or more";
+    // }
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    int v1, v2, v3;
+    
+    v1 = max(max(a, b) , max(b, c));
+    v2 = min(max(a, b) , max(b, c));
+    v3 = min(min(a, b) , min(b, c));
+
+    if (v1 < 100) cout << v1;
+    else if (v2 < 100) cout << v2;
+    else if (v3 < 100) cout << v3;
+    else cout << -1;
+    
+    return 0;
+}
+*/
