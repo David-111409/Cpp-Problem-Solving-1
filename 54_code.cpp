52c2b0803bc638468ac3e9f936d3bfa32d6ef5b6
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-01-21 00:05:24 +0200

    Create 54_code.cpp

---
+++ 54_code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+// HomeWork : Array Maximum
+// int arr_max(int arr[], int len,  double r = -INFINITY){
+    
+//     if ( len  == -1 ) return r;
+//     if (arr[len] > r) r = arr[len];
+    
+//     return arr_max(arr, len - 1, r);
+// }
+
+// sum Array
+// int sum(int ar[], int len){
+//     if (len == 0) return 0;
+//     return ar[len - 1] + sum(ar, len - 1);
+// }
+
+// average
+// double average(int ar[], double len, int i = 0){
+//     if (i == len) return 0 ;
+    
+//     return ar[i] / len  + average(ar, len ,   i + 1);
+// }
+
+
+int main()
+{
+    // int ar[] = {1, 8, 2, 10, 3};
+    // int s = sizeof(ar) / sizeof(ar[0]);
+    // cout << arr_max(ar, s-1);
+
+    // cout << sum(ar, s);
+    
+    // cout << average(ar,  s);
+    return 0;
+}
