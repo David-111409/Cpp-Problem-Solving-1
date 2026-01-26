100f6f75d81b9132f2f6619182cab87ca9f59140
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-01-26 21:56:18 +0200

    Update 55_code.cpp

--- 55_code.cpp
+++ 55_code.cpp
@@ -42,3 +42,54 @@ int main()
     for (int i = 0; i < n; i++) cout << ar[i] << " ";
     
 }
+
+/*
+
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{   int n;
+    cin >> n;
+    int ar[n];
+    
+    for (int i = 0; i < n; i++){
+        cin >> ar[i];
+    }
+    
+    // int current = ar[1] + 1;
+    // int left = ar[0] - 0;
+    // int minValue = left + current ;
+    // for (int j = 2; j < n; j++){
+    //     current = left + ar[j] + j;
+    //     minValue = min(current, minValue);
+    //     left = min(left, ar[j] - j);
+    // }
+    // cout << minValue;
+    
+    int first = INT_MAX;
+    int second = INT_MAX;
+    int third = INT_MAX;
+    
+    for (int el : ar){
+        if (el < first){
+            third = second;
+            second = first;
+            first = el;
+        } 
+        else if (el < second){
+            third = second;
+            second = el;
+            
+        }
+        else if (el < third) {
+            third = el;
+        }
+        
+        
+    }
+    cout << first << " " << second << " " << third;
+    
+    return 0;
+}
+*/
