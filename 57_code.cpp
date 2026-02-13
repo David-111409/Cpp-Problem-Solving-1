5f899e028b77be845efd2822e1abaaf4fda8ef1a
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-02-13 06:25:34 +0200

    Comment out multiple code sections in 57_code.cpp

--- 57_code.cpp
+++ 57_code.cpp
@@ -82,3 +82,150 @@ int main()
     }
     return 0;
 }
+
+
+/*
+
+#include <iostream>
+using namespace std;
+int main()
+{
+//     for (int i = 0; i <= 11; i++)
+//         cout << "*";
+    
+//     cout << endl;
+//   for (int i = 0; i<10;i++){
+//             cout << "*";
+            
+//     for (int i = 0; i < 10; i++){
+//         cout << " ";
+    
+//     }
+//     cout << "*\n";
+ 
+//   }
+    
+    
+    
+//   for (int i = 0; i <= 11; i++)
+//         cout << "*";
+    
+    //  for (int i = 0; i < 5; i++){
+    //      for (int j = 0; j < 5; j++){
+    //          if ( i == j || i + j == 4) cout << "*";
+    //          else cout << " ";
+    //      }
+    //      cout <<endl;
+    //  }
+    
+    // for (int i = 0; i < 11; i++){
+    //     for (int j = 0; j < 11; j++){
+    //         if (i == 0 || i == 10) cout << "*";
+    //         else if (j == 0 || j == 10) cout << "*";
+    //         else cout << " " ;
+    //     }
+    //     cout << endl;
+    // }
+    // int n = 5;
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++){
+    //         if (i == 0 || i == n-1) cout << "*";
+    //         else if (i == n - j - 1) cout<< "*";
+    //         else cout << " " ;
+    //     }
+    //     cout << endl;
+    // }
+    
+    // int n = 5;
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < n; j++){
+    //         if(i % 2 == 0) cout << "*";
+    //         else if (j == 0) cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    
+    // int n =  10;
+    // for (int i = 0; i< n; i++){
+    //     for (int j = 0; j< n; j++){
+    //         if (i == 0 || i == n - 1) cout << "*";
+    //         else if (j == 0) cout << "*";
+    //         else if (j == n - 1) cout << "*";
+    //         else if (i == j) cout << "*";
+    //         else if (i == n - j - 1) cout << "*";
+    //         else cout << " ";
+    //     }
+        
+    //     cout << endl;
+    // }
+    
+    //  int n =  10;
+    // for (int i = 0; i< n; i++){
+    //     for (int j = 0; j< n; j++){
+    //         if (j == 0) cout << "*";
+    //         else if (j == n - 1) cout << "*";
+    //         else if (i == j) cout << "*";
+    //         else cout << " ";
+    //     }
+        
+    //     cout << endl;
+    // }
+    
+    //  int n =  10;
+    // for (int i = 0; i< n; i++){
+    //     for (int j = 0; j< n; j++){
+    //         if (i == 0 || i == (n - 1)  / 2) cout << "*";
+    //         else if (j == 0) cout << "*";
+    //         // else if (j == n - 1) cout << "*";
+    //         // else if (i == j) cout << "*";
+    //         // else if (i == n - j - 1) cout << "*";
+    //         // else cout << " ";
+    //     }
+        
+    //     cout << endl;
+    // }
+    
+    //  int n =  10;
+    // for (int i = 0; i< n; i++){
+    //     for (int j = 0; j< n; j++){
+    //         if (i == 0 || i == n - 1) cout << "*";
+    //         else if (j == 0) cout << "*";
+    //         else if (j == n - 1) cout << "*";
+    //         else if (i == (n-1) / 2) cout << "*";
+           
+    //         else cout << " ";
+    //     }
+        
+    //     cout << endl;
+    // }
+    
+    // int n =  10;
+    // for (int i = 0; i< n; i++){
+    //     for (int j = 0; j< n; j++){
+    //         if (i == 0 || i == n - 1) cout << "*";
+    //         else if (j == 0) cout << "*";
+    //         else if (j == n - 1 && i < n / 2) cout << "*";
+    //         else if (i == (n-1) / 2) cout << "*";
+           
+    //         else cout << " ";
+    //     }
+        
+    //     cout << endl;
+    // }
+    
+     int n =  9;
+    for (int i = 0; i< n; i++){
+        for (int j = 0; j< n; j++){
+            if (i == 0 || i == n - 1) cout << "*";
+            else if (j == 0 && i < n / 2) cout << "*";
+            else if (j == n - 1  && i > n / 2) cout << "*";
+            else if (i == (n-1) / 2) cout << "*";
+           
+            else cout << " ";
+        }
+        
+        cout << endl;
+    }
+    return 0;
+}
+*/
